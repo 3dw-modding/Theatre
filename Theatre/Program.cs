888664e20a575e6064951de85640f94e48e962a9
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2023-08-21 15:24:31 -0600

    update chache and use up to date httpclient

--- Theatre/Program.cs
+++ Theatre/Program.cs
@@ -16,20 +16,24 @@ namespace Theatre
     {
         static void Main()
         {
-            string cacheDirectory = Path.Join("C:", "Theatre", "cache");
+            string cacheDirectory = Path.Join(
+                new FileInfo(Environment.ProcessPath!).Directory!.FullName,
+                "cache");
             string databaseFolderUrl = "https://github.com/Scyye/Theatre/raw/main/Databases/";
 
             Directory.CreateDirectory(cacheDirectory);
 
-            WebClient client = new WebClient();
+            using var client = new HttpClient();
 
 
             if (!File.Exists(Path.Join(cacheDirectory, "switch.bin")))
-                client.DownloadFile(databaseFolderUrl+"switch.bin", Path.Join(cacheDirectory, "switch.bin"));
+                File.WriteAllBytes(Path.Join(cacheDirectory, "switch.bin"), 
+                    client.GetByteArrayAsync(databaseFolderUrl + "switch.bin").GetAwaiter().GetResult());
 
 
             if (!File.Exists(Path.Join(cacheDirectory, "wiiu.bin")))
-                client.DownloadFile(databaseFolderUrl+"wiiu.bin", Path.Join(cacheDirectory, "wiiu.bin"));
+                File.WriteAllBytes(Path.Join(cacheDirectory, "wiiu.bin"),
+                    client.GetByteArrayAsync(databaseFolderUrl + "wiiu.bin").GetAwaiter().GetResult());
 
 
             CachedGBMods.AllWiiUModsCached = FileUtils.FromBytes(File.ReadAllBytes(Path.Join(cacheDirectory, "wiiu.bin")));
